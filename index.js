83129af4a56acae8f62b3018c6c9c68a0de0f417
Matt Bretl <mpbretl@gmail.com>
2019-10-09 10:39:18 -0500

    style: lint all .js files

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ module.exports = function PostGraphileConnectionFilterPostgisPlugin(
     // Check dependencies
     if (!build.versions) {
       throw new Error(
-        `Plugin ${pkg.name}@${
-          pkg.version
-        } requires graphile-build@^4.1.0 in order to check dependencies (current version: ${
-          build.graphileBuildVersion
-        })`
+        `Plugin ${pkg.name}@${pkg.version} requires graphile-build@^4.1.0 in order to check dependencies (current version: ${build.graphileBuildVersion})`
       );
     }
     const depends = (name, range) => {
